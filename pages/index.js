9409f4840d49465346c8cc49a4a97d339c5df61d
Tom Dent <tomdent991@hotmail.com>
2023-12-21 22:05:16 +0100

    a few issues, see ntoes

--- pages/index.js
+++ pages/index.js
@@ -2,79 +2,78 @@ import React, { useState, useEffect } from "react";
 
 export default function index() {
   const [message, setMessage] = useState("Loading");
-  const [image, setImage] = useState("")
-  const [text, setText] = useState("")
-  const [output, setOutput] = useState("")
+  const [image, setImage] = useState();
+  const [text, setText] = useState("");
+  const [output, setOutput] = useState("");
 
-  // useEffect(() => {
-  //   const response = fetch("http://localhost:3030/api/home")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       setMessage(data.message);
-  //     });
-  // }, []);
+  useEffect(() => {
+    console.log("image from useEffect: ", image);
+    extractText();
+  }, [image]);
+
+  useEffect(() => {
+    console.log("text from useEffect: ", text);
+    getInfoFromText(text);
+  }, [text]);
 
   function sendImage(event) {
     event.preventDefault();
     const response = fetch("http://localhost:3030/api/save", {
       method: "POST",
       headers: {
-        "Content-type": "image/jpeg"
+        "Content-type": "image/jpeg",
       },
-      body: image
-    }).then(() => {
-      console.log("image upload successful")
-      extractText();
-    }).catch(error => {
-      console.log(error)
-    });
+      body: image,
+    })
+      .then(() => {
+        console.log("image upload successful");
+        setImage(image);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   async function extractText() {
-    const response = fetch("http://localhost:3030/api/extract")
-    .then((response) => response.json()).then((data) => {
-      console.log(data)
-      setText(data.text)
-      getInfoFromText(data.text)
-    })
-  }
+    const response = await fetch("http://localhost:3030/api/extract")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response data: ", data.text);
+        setText(data.text);
+      });
+    }
+    console.log("TExt: ", text)
 
-  async function getInfoFromText(extractText) {
-    if (extractText === "") return;
+  async function getInfoFromText(text) {
+    if (text === "") return;
 
-    console.log(typeof extractText, extractText)
+    console.log(typeof text, text);
 
     const response = await fetch("http://localhost:3030/api/getInfo", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: extractText,
+      body: JSON.stringify({ text }),
     });
 
     const data = await response.json();
     const { output } = data;
     console.log("OpenAI replied...", output);
-    // setIsLoading(false)
-    setOutput(output)
+    setOutput(output);
   }
 
-  // useEffect(() => {
-  //   const response = fetch("http://localhost:3030/api/save")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data)
-  //       setMessage(data.text)
-  //     });
-  // }, []);
 
   return (
     <>
       <p>{message}</p>
       <form onSubmit={sendImage}>
-      <input type="file" name="image" onChange={(event) => setImage(event.target.files[0])}></input>
-      <button type="submit">Submit</button>
+        <input
+          type="file"
+          name="image"
+          onChange={(event) => setImage(event.target.files[0])}
+        ></input>
+        <button type="submit">Submit</button>
       </form>
       <p>{text ? text : null}</p>
       <p>{output ? output : null}</p>
